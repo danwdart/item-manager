a6f4181e6b25d2ce25d385b09467be963630ce50
Dan Dart <dan.dart@4cplatform.co.uk>
2021-03-15 00:59:36 +0000

    nix files & cabal file

--- default-fix-ghcjs.nix
+++ default-fix-ghcjs.nix
@@ -1,27 +1,22 @@
-{ system ? builtins.currentSystem, nixpkgs ? import <nixpkgs> {} }:
+{ system ? builtins.currentSystem }:
 (import ./external/reflex-platform {
   inherit system;
   config.android_sdk.accept_license = true;  
 }).project ({ pkgs, ... }: {
   packages = {
+    common = ./app/common;
     backend = ./app/backend;
     frontend = ./app/frontend;
-    common = ./app/common;
   };
 
   shellToolOverrides = ghc: super: {
-    inherit (ghc) apply-refact stylish-haskell wai-app-static;
+    # inherit (ghc) wai-app-static;
     # wai-app-static = pkgs.haskellPackages.wai-app-static;
   };
 
   overrides = self: super: {
     # Remove when using ghc wai/warp
-    # mmorph = self.callHackage "mmorph" "1.1.3" {};
-
-    # todo no doctests on later ghc
-    # ghc = nixpkgs.haskell.compiler.ghc884; # because some of these libraries are not yet ready for 901?
-    # backend = self.callCabal2nix "backend" ./backend {};
-    servant-quickcheck = self.callHackage "servant-quickcheck" "0.0.7.4" {}; # 0.0.10.0
+    mmorph = self.callHackage "mmorph" "1.1.3" {}; 
   };
 
   useWarp = true;
