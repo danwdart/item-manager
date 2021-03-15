672676da5fe2970685fabb31d7ce8b37816bfcf5
Dan Dart <dan.dart@4cplatform.co.uk>
2021-03-15 01:06:13 +0000

    Frontend Category services

---
+++ app/frontend/src/Service/Category.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedLists   #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UnicodeSyntax     #-}
+{-# OPTIONS_GHC -Wno-simplifiable-class-constraints #-}
+
+module Service.Category where
+
+import           Data.Aeson
+import           Data.Aeson.Text
+import qualified Data.ByteString.Lazy   as BL
+import           Data.Maybe
+import           Data.Text              (Text)
+import qualified Data.Text              as T
+import           Data.Text.Encoding
+import qualified Data.Text.Lazy         as LT
+import qualified Data.Text.Lazy.Builder as B
+import           GHC.Generics
+
+import           Reflex.Dom
+import           Reflex.Dom.Xhr
+
+import Types.Category
+
+apiEndpoint ∷ Text
+apiEndpoint = "http://localhost:8081/api"
+
+-- Widget, really??
+
+getAllCategories ∷ MonadWidget t m ⇒ Event t () → m (Event t CategoriesResponse)
+getAllCategories e = fmap (fromMaybe [Category 0 "Cannot fetch results"]) <$> getAndDecode ((apiEndpoint <> "/categories") <$ e)
+
+getCategory ∷ MonadWidget t m ⇒ Event t Int → m (Event t (Maybe Category))
+getCategory e = getAndDecode ((\categoryId' -> apiEndpoint <> "/categories/" <> T.pack (show categoryId')) <$> e)
+
+createCategory ∷ MonadWidget t m ⇒ Event t Text → m (Event t (Maybe Category))
+createCategory e = fmap decodeXhrResponse <$> performRequestAsync (
+    postJson (apiEndpoint <> "/categories") . CreateCategory <$> e
+    )
+
+deleteCategory ∷ MonadWidget t m ⇒ Event t Int → m (Event t (Maybe ()))
+deleteCategory e = fmap decodeXhrResponse <$> performRequestAsync (
+    (\categoryId' -> XhrRequest "DELETE" (apiEndpoint <> "/categories/" <> T.pack (show categoryId')) def) <$> e
+    )
+
+modifyCategory ∷ MonadWidget t m ⇒ Event t Category → m (Event t (Maybe Category))
+modifyCategory e = fmap decodeXhrResponse <$> performRequestAsync (
+    (\category -> XhrRequest "PUT" (apiEndpoint <> "/categories/" <> T.pack (show (categoryId category))) $ def {
+        _xhrRequestConfig_headers = "Content-type" =: "application/json",
+        _xhrRequestConfig_sendData = LT.unpack . B.toLazyText . encodeToTextBuilder $ toJSON category
+    }) <$> e)
---
+++ app/frontend/src/Types/Category.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Types.Category where
+
+import Data.Aeson
+import Data.Text
+import GHC.Generics
+
+newtype CreateCategory = CreateCategory {
+    createCategoryName :: Text
+} deriving (Generic, ToJSON, Show)
+
+data Category = Category {
+    categoryId :: Int,
+    name  :: Text
+} deriving (Generic, Show)
+
+instance FromJSON Category where
+    parseJSON (Object o) = Category <$> o .: "id" <*> o .: "name"
+    parseJSON _ = error "Incorrect use of parsing a category"
+
+instance ToJSON Category where
+    toJSON (Category id' name') = object ["id" .= id', "name" .= name']
+
+type CategoriesResponse = [Category]
