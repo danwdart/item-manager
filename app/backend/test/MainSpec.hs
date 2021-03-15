2876452e47eb97ea835196879c13d78b4a48ce43
Dan Dart <dan.dart@4cplatform.co.uk>
2021-03-15 01:03:07 +0000

    More bits of skeleton

---
+++ app/backend/test/MainSpec.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE UnicodeSyntax #-}
+module MainSpec where
+
+import           Test.Hspec              (HasCallStack, Spec, describe, it,
+                                          parallel, runIO, shouldBe,
+                                          shouldSatisfy, xdescribe)
+import           Test.Hspec.Expectations (shouldNotBe, shouldNotContain)
+
+spec ∷ Spec
+spec = pure ()
