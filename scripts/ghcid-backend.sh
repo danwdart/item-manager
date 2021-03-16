3ae1b444b15a33d5852a92138fb9b560720cc840
Dan Dart <dan.dart@4cplatform.co.uk>
2021-03-16 19:44:14 +0000

    When to restart ghcid

--- scripts/ghcid-backend.sh
+++ scripts/ghcid-backend.sh
@@ -4,5 +4,7 @@
 cabal exec ghcid -- -c "cabal repl backend" \
     --restart app/common/common.cabal \
     --restart app/backend/backend.cabal \
+    --restart scripts \
     --reload app/common/src \
+    --reload app/backend/db \
     -aT Main.main
