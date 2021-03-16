a8344c231f81c0b96044d4b3c69be1ae6ad6126e
Dan Dart <dan.dart@4cplatform.co.uk>
2021-03-16 22:52:54 +0000

    Dropdowns and everything work now

--- app/frontend/src/Main.hs
+++ app/frontend/src/Main.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
 module Main where
@@ -32,13 +33,21 @@ categoryToMap categories = M.fromList $ fmap mapper categories where
           ]
       )
 
+categoriesToSelect :: [Category] -> Map Int Text
+categoriesToSelect categories = M.fromList $ fmap mapper categories where
+    mapper category = (
+      Category.categoryId category,
+      Category.name category
+      )
+
 itemToMap :: [Item] -> Map Text (Map Text Text)
 itemToMap categories = M.fromList $ fmap mapper categories where
     mapper item = (
         T.pack . show . itemId $ item,
         M.fromList [
           ("id", T.pack . show . itemId $ item),
-          ("name", Item.name item)
+          ("name", Item.name item),
+          ("categoryId", T.pack . show $ Item.categoryId item)
           ]
       )
 
@@ -47,6 +56,10 @@ widgetItems = mdo
   epb <- getPostBuild
 
   eGetItems <- getAllItems $ leftmost [epb, evCloseAddModal, evCloseEditModal, refreshWhen]
+
+  eGetCategories <- getAllCategories $ leftmost [epb, addItem, void editWhen]
+
+  dynCategories <- holdDyn [] $ categoriesToSelect <$> eGetCategories
 
   dynTableData <- holdDyn [] $ itemToMap <$> eGetItems
 
@@ -73,7 +86,8 @@ widgetItems = mdo
         pure (never, never)
       ),
       ("Category", \k v -> do
-        dynText $ fmap (M.findWithDefault "(none)" "category") v
+        let dynCategoryId = read @Int . T.unpack <$> fmap (M.findWithDefault "(none)" "categoryId") v
+        dynText $ M.findWithDefault "(None)" <$> dynCategoryId <*> dynCategories
         pure (never, never)
       ),
       ("", \k v -> do
@@ -101,15 +115,19 @@ widgetItems = mdo
         divClass "modal-header" .
           elClass "h5" "modal-title" $
             text "Add"
-        elItem <- divClass "modal-body" .
-          el "form" .
-            divClass "form-group" $ do
+        (elItemName, elCategory) <- divClass "modal-body" .
+          el "form" $ do
+            elItemName <- divClass "form-group" $ do
               elAttr "label" [("for", "name")] $ text "Name"
               inputElement $ def & inputElementConfig_elementConfig . elementConfig_initialAttributes .~ [("id", "name"), ("class", "form-control"), ("placeholder", "Bob")]
+            elCategory <- divClass "form-group" $ do
+              elAttr "label" [("for", "categoryId")] $ text "Category"
+              dropdown 0 dynCategories $ def & dropdownConfig_attributes .~ pure [("id", "categoryId"), ("class", "form-control")]
+            pure (elItemName, elCategory)
         divClass "modal-footer" $ do
           evtSaveAddModal <- bsButton "btn btn-primary" "Save"
-          let dynItemName = _inputElement_value elItem
-          let dynCategoryId = pure 0
+          let dynItemName = _inputElement_value elItemName
+          let dynCategoryId = _dropdown_value elCategory
           let dynCreateItem = CreateItem <$> dynItemName <*> dynCategoryId
           evtCreate <- createItem $ tagPromptlyDyn dynCreateItem evtSaveAddModal
           evtCloseModal <- bsButton "btn btn-secondary" "Cancel"
@@ -130,24 +148,39 @@ widgetItems = mdo
           elClass "h5" "modal-title" $
             text "Edit"
         (dynItemId, dynItemName, dynItemCategoryId) <- divClass "modal-body" .
-          el "form" .
-            divClass "form-group" $ mdo
+          el "form" $ do
+            evtGetItem <- getItem (read . T.unpack <$> editWhen)
+
+            let evtItemName = maybe "" Item.name <$> evtGetItem
+            let evtItemId = maybe 0 itemId <$> evtGetItem
+            let evtCategoryId = maybe 0 Item.categoryId <$> evtGetItem
+
+            dynItemName <- holdDyn "" evtItemName
+            dynItemId <- holdDyn 0 evtItemId
+            dynCategoryId <- holdDyn 0 evtCategoryId
+
+            inputElName <- divClass "form-group" $ do
               elAttr "label" [("for", "name")] $ text "Name"
-              evtGetItem <- getItem (read . T.unpack <$> editWhen)
-              let evtGrabbedItem = maybe "" Item.name <$> evtGetItem
-              let evtItemId = maybe 0 itemId <$> evtGetItem
-              dynItemName <- holdDyn "" evtGrabbedItem
-              let dynItemCategoryId = pure 0
-              dynItemId <- holdDyn 0 evtItemId
-              inputItem <- inputElement $ def &
+              inputElement $ def &
                 inputElementConfig_elementConfig . elementConfig_initialAttributes .~ [
                   ("id", "name"),
                   ("class", "form-control"),
                   ("placeholder", "Bob's Item")
                   ] &
                 inputElementConfig_setValue .~ updated dynItemName
-              let dynItemNameText = _inputElement_value inputItem
-              pure (dynItemId, dynItemNameText, dynItemCategoryId)
+
+            let dynItemNameText = _inputElement_value inputElName
+
+            inputElCategory <- divClass "form-group" $ do
+              elAttr "label" [("for", "categoryId")] $ text "Category"
+              dropdown 0 dynCategories $ def &
+                dropdownConfig_attributes .~ pure [("id", "categoryId"), ("class", "form-control")] &
+                dropdownConfig_setValue .~ updated dynCategoryId
+
+            let dynItemCategoryId = _dropdown_value inputElCategory
+            
+            pure (dynItemId, dynItemNameText, dynItemCategoryId)
+
         divClass "modal-footer" $ do
           evtSaveModal <- bsButton "btn btn-primary" "Save"
           let dynItem = Item <$>
