5980f8e835484523e41b8172ae6b359045ebc8c9
Dan Dart <dan.dart@4cplatform.co.uk>
2021-03-17 03:50:00 +0000

    Frontend type tests

---
+++ app/frontend/test/Types/ItemSpec.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE UnicodeSyntax #-}
+module Types.ItemSpec where
+
+import           Data.Aeson
+import           Test.Hspec                     (HasCallStack, Spec, describe,
+                                                 it, parallel, runIO, shouldBe,
+                                                 shouldSatisfy, xdescribe)
+import           Test.Hspec.Expectations        (shouldNotBe, shouldNotContain)
+import           Test.QuickCheck
+import           Test.QuickCheck.Instances.Text
+import           Types.Category                 as Category
+import           Types.CategorySpec             hiding
+                                                 (propSerialiseUnserialise)
+import           Types.Item
+
+instance Arbitrary Item where
+    arbitrary = Item <$> arbitrary <*> arbitrary <*> arbitrary
+
+propSerialiseUnserialise ∷ Item → Property
+propSerialiseUnserialise item =
+    decode (encode item) === Just item
+
+spec ∷ Spec
+spec = describe "Item" .
+    it "serialises to JSON and unserialises" $
+        quickCheck propSerialiseUnserialise
